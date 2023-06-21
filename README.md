32c763a5ddead44210811fcb1acb566123c4c2df
Viet Duc <ducxjt18@gmai.com>
2023-06-21 19:43:55 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+// build tool -> tool chain
+// vite
+
+// cau lenh cmd
+// tao project (tao thu muc du an tai duong dan hien tai)
+// npm i create vite@latest
+
+// cai dat cac packet da dc
+
+// cau truc thu muc
+// node_modules => thu muc cai dat dependencies
+// public => chon cac files tinh cho pheop truy cap truc tiep
+// src => chua cac sources code du an
+
+// ES6 Modules
+// co moi file (.) la mot module, va co context rieng, khong anh huong den nhau
+// de lien cac module su dung cup phap (import / export)
+// .exprot (xuat ra nhung gia tri, phuong thuc,... cho phep cac module khac co the truy cap duoc)
+// .import (nhap/ truy cap vao cac gia tri, phuong thuc,.. duoc khai bao va exprot trong cac export khac)
+
+
+// Export 
+// export/import default : moi module chi duoc phep export default 1 gia tri
+// export/import theo ten : theo dung gia tri ten
+
+// Tinh nang
+/** 
+ * 1. JSX
+ * 2. Component-based => la mot ham tra ve 1 doan JSX xac dinh noi dung hien thi tren man hinh
+ * 3. Virtual DOM
+ * 4. Ho tro da nen tang
+ * 
+ * Nhuoc diem cua DOM thuan:
+ * . Cac thao tac cuc ky ton kem
+ * 
+ * Virtual DOM la mo hinh doi tuong dai dien cho cau truc tuong ung voi DOM that nhung ma trong bo nho 
+ * 
+ * JSX 
+ * Cac luu y khi su dung cu phap JSX:
+ * . Cu phap mo rong 
+ */
---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-item">
+          <div className="card-thumbnail">
+            <img
+              src="https://media.techmaster.vn/api/static/bub3enc51co7s932dsk0/5lZVsI6H"
+              alt=""
+            />
+          </div>
+          <div className="card-body">
+            <div className="card-title">
+              6 lợi ích của việc sở hữu chứng chỉ FE và cơ hội làm việc tại Nhật
+              Bản
+            </div>
+            <p className="card-text">
+              Sở hữu chứng chỉ FE sẽ được ưu tiên xét visa làm việc trong lĩnh
+              vực CNTT t�...
+            </p>
+          </div>
+          <div className="card-footer">
+            <div className="view-count">51 Lượt xem</div>
+            <div className="published-at">20-06-2023</div>
+          </div>
+        </div>
+        <div className="card-item">
+          <div className="card-thumbnail">
+            <img
+              src="https://media.techmaster.vn/api/static/OH6Sopmw/ZZHJz2jg"
+              alt=""
+            />
+          </div>
+          <div className="card-body">
+            <div className="card-title">10 lỗi sai thường gặp trong CSS</div>
+            <p className="card-text">
+              CSS được phát triển bởi W3C (World Wide Web Consortium) vào năm
+              1996, vì HTML không đ�...
+            </p>
+          </div>
+          <div className="card-footer">
+            <div className="view-count">51 Lượt xem</div>
+            <div className="published-at">20-06-2023</div>
+          </div>
+        </div>
+        <div className="card-item">
+          <div className="card-thumbnail">
+            <img
+              src="https://media.techmaster.vn/api/static/OH6Sopmw/FlNkxp3b"
+              alt=""
+            />
+          </div>
+          <div className="card-body">
+            <div className="card-title">
+              7 thủ thuật JavaScript bạn nên biết
+            </div>
+            <p className="card-text">
+              Javascript tips and tricks to Optimize Performance
+            </p>
+          </div>
+          <div className="card-footer">
+            <div className="view-count">51 Lượt xem</div>
+            <div className="published-at">20-06-2023</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// bieu thuc {} => nhung bat ky bieu thuc Js hop le
